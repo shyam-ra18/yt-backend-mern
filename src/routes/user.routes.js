483090306e81ddc4d467a252c73c8a5f299de2af
Shyam Radadiya <shyam.radadiya100@gmail.com>
2024-01-12 16:52:08 +0530

    update user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   logOutUser,
   loginUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -22,7 +29,23 @@ router.post("/login", loginUser);
 
 //Secured Routes
 router.post("/logout", verifyJWT, logOutUser);
-
 router.post("/refresh-token", refreshAccessToken);
+router.post("/changePassword", verifyJWT, changeCurrentPassword);
+router.get("/currentUser", verifyJWT, getCurrentUser);
+router.patch("/updateAccount", verifyJWT, updateAccountDetails);
+router.patch(
+  "/updateAvatar",
+  verifyJWT,
+  upload.single("avatar"),
+  updateUserAvatar
+);
+router.patch(
+  "/updateCoverImage",
+  verifyJWT,
+  upload.single("coverImage"),
+  updateUserCoverImage
+);
+router.get("/channel/:username", verifyJWT, getUserChannelProfile);
+router.get("/history", verifyJWT, getWatchHistory);
 
 export default router;
